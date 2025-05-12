1c12e19104710fecc247e43cb9daf34228873801
Felipanjos <61696148+Felipanjos@users.noreply.github.com>
2025-05-12 12:59:31 -0300

    implement decidirTipoDeAtividadePorPorte

--- trabalho.js
+++ trabalho.js
@@ -7,6 +7,19 @@ export function calcularConsumoDeRacao(nome, idade, peso) {
   return (peso * 30) * 10;
 }
 
+export function decidirTipoDeAtividadePorPorte(porte) {
+  switch (porte) {
+    case 'pequeno':
+      return 'brincar dentro de casa';
+    case 'medio':
+      return 'caminhada no quarteirao';
+    case 'grande':
+      return 'correr no parque';
+    default:
+      return 'porte invalido';
+  }
+}
+
 export async function buscarDadoAsync() {
   return await 'Pipoca';
 }
