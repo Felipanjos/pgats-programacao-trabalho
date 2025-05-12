b92e3841a500d8e356d127a0cb9ab697f32c5b9c
Felipanjos <61696148+Felipanjos@users.noreply.github.com>
2025-05-12 13:08:25 -0300

    implement verificarSePodeSerAdotado

--- trabalho.js
+++ trabalho.js
@@ -2,6 +2,16 @@ export const geradorDeTagsDeIdentificacao = (nome) => {
   return nome.toUpperCase();
 };
 
+export function verificarSePodeSerAdotado(idade, porte) {
+  const IDADE_MINIMA = 2;
+
+  const podeSerAdotado =
+    porte === 'P' ||
+    (porte === 'M' && idade >= 1) ||
+    idade >= IDADE_MINIMA;
+
+  return podeSerAdotado;
+}
 
 export function calcularConsumoDeRacao(nome, idade, peso) {
   return (peso * 30) * 10;
