72f2ad3b1a12d31efbb77a18b6ab0fd1ddaea24c
Felipanjos <61696148+Felipanjos@users.noreply.github.com>
2025-05-12 12:59:07 -0300

    implement calcularConsumoDeRacao

--- trabalho.js
+++ trabalho.js
@@ -2,6 +2,11 @@ export const geradorDeTagsDeIdentificacao = (nome) => {
   return nome.toUpperCase();
 };
 
+
+export function calcularConsumoDeRacao(nome, idade, peso) {
+  return (peso * 30) * 10;
+}
+
 export async function buscarDadoAsync() {
   return await 'Pipoca';
 }
